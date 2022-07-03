1f152217598da00564f485389c1abeae7fe73a0a
FriedAge <107921193+FriedAge@users.noreply.github.com>
2022-07-03 21:54:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -193,6 +193,7 @@ async def on_message(message):
                 message.guild.voice_client.play(source)
     await client.process_commands(message)
 
+'''
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None:
@@ -236,6 +237,7 @@ async def on_voice_state_update(member, before, after):
                     await member.guild.voice_client.disconnect()
                     await asyncio.sleep(0.5)
                     await after.channel.connect()
+'''
 
 @client.event
 async def on_command_error(ctx, error):
